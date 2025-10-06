55163ab62e87011fdc32a3754900c60546b9edc8
Guisielo <155173663+Guisielo@users.noreply.github.com>
2025-10-06 15:10:33 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import "bootstrap";
-import "./style.css";
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensiones=['.com', '.net','.us','.io','.es'];
 
+  for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let ext of extensiones) {
+        console.log(p + a + n + ext);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+        // "Domain hack": si el sustantivo termina igual que la extensión, lo cortamos
+        if (n.endsWith(ext.replace('.', ''))) {
+          let hacked = p + a + n.slice(0, n.length - ext.length + 1) + ext;
+          console.log(hacked + "  <-- hackeado 😎");
+        }
+      }
+    }
+  }
+}
